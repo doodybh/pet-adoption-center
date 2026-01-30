3df30d42fd96c5526436438241af1a4a0bbd43c6
doodybh <doodybhr38@gmail.com>
2026-01-30 07:26:02 +0300

    made it be able to view pets even while not signed in

--- middleware/is-signed-in.js
+++ middleware/is-signed-in.js
@@ -1,6 +1,9 @@
 const isSignedIn = (req, res, next) => {
+  if (req.path === "/pets" && req.method === "GET") {
+    return next();
+  }
   if (req.session.user) return next();
   res.redirect("/auth/sign-in");
-};
+}; 
 
 module.exports = isSignedIn;
